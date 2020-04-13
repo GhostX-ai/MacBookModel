2327393732bd8f2df7d61cb938f5481dff409fe2
GhostX <egamovkhurshed29@gmail.com>
2020-04-13 18:14:47 +0500

    Project inited and Model has been added

---
+++ Models.cs
@@ -0,0 +1,9 @@
+using System;
+class MacBook
+{
+    public int Version { get; set; }
+    public int Display { get; set; }
+    public string Processor { get; set; }
+    public int RAM { get; set; }
+    public int Memory { get; set; }
+}
